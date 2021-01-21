bd74a83c21cac479b23b3d357759fca0adb9a485
Ashir420 <77463601+Ashir420@users.noreply.github.com>
2021-01-20 20:49:31 -0500

    Add files via upload

---
+++ Week 1 - Not so-simple Hello World/Ashir.py
@@ -0,0 +1,16 @@
+print("Hello World")
+def personal_details () :
+    name , age = "Ashir Ahmed" , 14
+    address = "Brampton, Ontario"
+    print("Name: {}\nage: {}\naddress: {}".format(name, age, address))
+    print("Favorite food: Cheese Pizza")
+
+    print("Favorite rapper: Juice Wrld  R.I.P")
+
+    print("Favorite course: math and science")
+    
+    print("Follow my instagram: @axxir06")
+
+personal_details()
+print("I love coding!!")
+print("Have a nice day!")
