02fa13aaa66ae50cf18e74d14af1b24b267d9836
Kuvalbrar <77647838+Kuvalbrar@users.noreply.github.com>
2021-02-03 20:17:58 -0500

    Add files via upload

---
+++ meme_text_kuval.py
@@ -0,0 +1,9 @@
+meme = input("input word you want to memefy:")
+
+meme_lower = meme.lower()
+meme_uppercase = meme.upper()
+
+here = meme_lower[0] + meme_uppercase[1] + meme_lower[2] + meme_uppercase[3]
+
+print(here)
+
