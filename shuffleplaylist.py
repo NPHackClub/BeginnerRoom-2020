50f138e2f44ec75b80780faa89ea3e2fff7b31e5
fizzirule <74218749+fizzirule@users.noreply.github.com>
2021-02-11 15:57:38 -0500

    shuffleplaylist-fizzah

---
+++ shuffleplaylist.py
@@ -0,0 +1,8 @@
+song1 = input("Insert your first song: ")
+song2 = input("Insert your second song: ")
+song3 = input("Insert your third song: ")
+shuffle = [song1,song2]
+shuffle.sort()
+shuffle1 = [song1,song2,song3]
+shuffle1.reverse()
+print(shuffle1)
