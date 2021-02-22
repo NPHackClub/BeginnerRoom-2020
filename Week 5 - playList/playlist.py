32903f6917a236fe685106c148b8531c62210f1f
Jasleenk47 <77461821+Jasleenk47@users.noreply.github.com>
2021-02-22 17:28:00 -0500

    Jasleen-Playlist

---
+++ Week 5 - playList/playlist.py
@@ -0,0 +1,14 @@
+import random
+
+song_playlist = ["Test Drive, by Ariana Grande" , "Formula, by Labrinth" , "Habits, by Labrinth"]
+
+print(song_playlist)
+
+song_playlist.sort()
+print(song_playlist)
+
+song_playlist.reverse()
+print(song_playlist)
+
+random.shuffle(song_playlist)
+print(song_playlist)
